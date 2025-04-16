5cf623f353e5340da6fdd96514f39ebecca97718
Pooja Shah <53046887+pooja0805@users.noreply.github.com>
2025-04-16 23:18:50 +0530

    Added entry of repo

--- src/classes/package_search.py
+++ src/classes/package_search.py
@@ -89,6 +89,7 @@ class PackageSearch:
                         cachedPackage["P"] = pkg["packageName"]
                         cachedPackage["S"] = cachedPackage["P"].lower().upper()
                         cachedPackage["V"] = pkg["version"]
+                        cachedPackage["repo"] = pkg.get("repo", "N/A")
                         try:
                             cachedPackage["B"] = cls.DISTRO_BIT_MAP[distroName][distroVersion]
                         except Exception as e:
