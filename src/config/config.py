0a703bc87445277dc7c8ecbb95a3650854f7cd6b
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2025-06-03 18:49:33 +0530

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -41,7 +41,7 @@ SUPPORTED_DISTROS = {
       "Ubuntu 22.04":"Ubuntu_22_04_Package_List.json",
       "Ubuntu 24.04":"Ubuntu_24_04_Package_List.json",
       "Ubuntu 24.10":"Ubuntu_24_10_Package_List.json",
-      "Ubuntu 24.05":"Ubuntu_25_05_Package_List.json"
+      "Ubuntu 24.05":"Ubuntu_25_04_Package_List.json"
    },
    "SUSE Linux Enterprise Server":{
       "SLES 12 SP3":"Suse_Linux_Enterprise_Server_12_SP3_Package_List.json",
